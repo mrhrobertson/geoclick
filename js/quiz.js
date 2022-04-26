863ba8837533d65efd6ad64fc9df7b123a2f1d11
Mark Robertson <mark.rh.robertson@gmail.com>
2022-04-26 20:52:36 +0100

    More work done to mobile answers.

--- js/quiz.js
+++ js/quiz.js
@@ -1,6 +1,9 @@
 const play = document.getElementById("play");
 const quiz = document.getElementById("quiz");
+const next = document.getElementById("next");
 const counter = document.getElementById("score");
+const question_loc =
+  "https://mrhrobertson.github.io/geoquiz/assets/questions.json";
 
 let score =
   window.localStorage.getItem("score") == null
@@ -23,30 +26,38 @@ function start_quiz() {
   play.classList.add("hidden");
   quiz.classList.remove("hidden");
   window.localStorage.setItem("score", 0);
+  score = window.localStorage.getItem("score");
   counter.innerText = score;
   load_question();
 }
 
+function add_score(num) {
+  score = parseInt(score) + num;
+  window.localStorage.setItem("score", score);
+  counter.innerText = score;
+}
+
 async function load_question() {
-  let questions = await fetch("../assets/questions.json").then((res) =>
-    res.json()
-  );
+  let questions = await fetch(question_loc).then((res) => res.json());
   let loaded = await questions["questions"][question];
   let content = loaded.question;
+  next.classList.add("hidden");
   document.getElementById("q_id").innerText = parseInt(question) + 1;
   document.getElementById("q_content").innerHTML = content;
 }
 
-async function answer_question(answer) {
-  let questions = await fetch("../assets/questions.json").then((res) =>
-    res.json()
-  );
+async function answer_question(answer, e) {
+  let questions = await fetch(question_loc).then((res) => res.json());
   let loaded = await questions["questions"][question];
-  if (answer == loaded.correct) {
-    console.log("Correct! :D");
-    question++;
-    score++;
-  } else {
-    console.log("Incorrect! D:");
+  if ((loaded.type = "map")) {
+    if (answer == loaded.correct) {
+      e.target.classList.add("correct");
+      console.log("Correct! :D");
+      add_score(1);
+      question++;
+      if (next.classList.contains("hidden")) next.classList.remove("hidden");
+    } else {
+      console.log("Incorrect! D:");
+    }
   }
 }
